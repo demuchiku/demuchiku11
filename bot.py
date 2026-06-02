fcba6e731d65acbddea35ecd6c629654453e84e0
demuchiku <dzordanloh@gmail.com>
2026-06-03 00:26:42 +0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import os
 TOKEN = os.environ.get("8787106530:AAGsSIQb2ta3pYPbX-aeWYuyREgaz4mId_8")
 OPENROUTER_KEY = os.environ.get("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")
 
-bot = telebot.TeleBot(8787106530:AAGsSIQb2ta3pYPbX-aeWYuyREgaz4mId_8)
+bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
